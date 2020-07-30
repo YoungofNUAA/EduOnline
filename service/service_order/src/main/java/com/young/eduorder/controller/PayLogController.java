44d8313be6ef9506fc7b18a0954a4cdd68d302ab
YoungofNUAA <linghui_yang@163.com>
2020-07-30 18:04:34 +0800

    在线教育项目第15天 微信支付 2020/07/30

--- service/service_order/src/main/java/com/young/eduorder/controller/PayLogController.java
+++ service/service_order/src/main/java/com/young/eduorder/controller/PayLogController.java
@@ -1,9 +1,16 @@
 package com.young.eduorder.controller;
 
 
+import com.young.commonutils.R;
+import com.young.eduorder.service.PayLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -14,8 +21,32 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-07-29
  */
 @RestController
-@RequestMapping("/eduorder/pay-log")
+@RequestMapping("/eduorder/paylog")
 public class PayLogController {
+    @Autowired
+    private PayLogService payLogService;
 
+    @GetMapping("createNative/{orderNo}")
+    public R createNative(@PathVariable String orderNo){
+
+        Map map = payLogService.createNative(orderNo);
+        return R.ok().data(map);
+    }
+
+    //查询订单状态
+    @GetMapping("queryPayStatus/{orderNo}")
+    public R queryPayStatus(@PathVariable String orderNo){
+        Map<String,String> map = payLogService.queryPayStatus(orderNo);
+
+        if(map==null){
+            return R.error().message("支付出错了");
+        }
+
+        if(map.get("trade_state").equals("SUCCESS")){
+            payLogService.updateOrderStatus(map);
+            return R.ok().message("支付成功");
+        }
+        return R.ok().message("支付中");
+    }
 }
 
--- service/service_order/src/main/java/com/young/eduorder/service/impl/PayLogServiceImpl.java
+++ service/service_order/src/main/java/com/young/eduorder/service/impl/PayLogServiceImpl.java
@@ -1,10 +1,23 @@
 package com.young.eduorder.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.github.wxpay.sdk.WXPayUtil;
+import com.young.eduorder.Utils.HttpClient;
+import com.young.eduorder.entity.Order;
 import com.young.eduorder.entity.PayLog;
 import com.young.eduorder.mapper.PayLogMapper;
+import com.young.eduorder.service.OrderService;
 import com.young.eduorder.service.PayLogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.young.servicebase.exceptionhandler.GuliException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -17,4 +30,93 @@ import org.springframework.stereotype.Service;
 @Service
 public class PayLogServiceImpl extends ServiceImpl<PayLogMapper, PayLog> implements PayLogService {
 
+    @Autowired
+    private OrderService orderService;
+
+    @Override
+    public Map createNative(String orderNo) {
+        try{
+            QueryWrapper<Order> wrapper = new QueryWrapper<>();
+            wrapper.eq("order_no",orderNo);
+            Order order = orderService.getOne(wrapper);
+
+            Map m = new HashMap();
+            m.put("appid", "wx74862e0dfcf69954");
+            m.put("mch_id", "1558950191");
+            m.put("nonce_str", WXPayUtil.generateNonceStr());
+            m.put("body", order.getCourseTitle());
+            m.put("out_trade_no", orderNo);
+            m.put("total_fee", order.getTotalFee().multiply(new BigDecimal("100")).longValue()+"");
+            m.put("spbill_create_ip", "127.0.0.1");
+            m.put("notify_url", "http://guli.shop/api/order/weixinPay/weixinNotify\n");
+            m.put("trade_type", "NATIVE");
+
+            HttpClient client = new HttpClient("https://api.mch.weixin.qq.com/pay/unifiedorder");
+            client.setXmlParam(WXPayUtil.generateSignedXml(m,"T6m9iK73b0kn9g5v426MKfHQH7X8rKwb"));
+            client.setHttps(true);
+            client.post();
+
+            String content = client.getContent();
+            //XML--->Map
+            Map<String,String> resultMap = WXPayUtil.xmlToMap(content);
+
+            Map map = new HashMap<>();
+            map.put("out_trade_no", orderNo);
+            map.put("course_id", order.getCourseId());
+            map.put("total_fee", order.getTotalFee());
+            map.put("result_code", resultMap.get("result_code"));
+            map.put("code_url", resultMap.get("code_url"));
+            return map;
+
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new GuliException(20001,"出错啦");
+        }
+    }
+
+
+    @Override
+    public Map<String, String> queryPayStatus(String orderNo) {
+        try{
+            Map m = new HashMap<>();
+            m.put("appid", "wx74862e0dfcf69954");
+            m.put("mch_id", "1558950191");
+            m.put("out_trade_no", orderNo);
+            m.put("nonce_str", WXPayUtil.generateNonceStr());
+
+            HttpClient client = new HttpClient("https://api.mch.weixin.qq.com/pay/orderquery");
+            client.setXmlParam(WXPayUtil.generateSignedXml(m,"T6m9iK73b0kn9g5v426MKfHQH7X8rKwb"));
+            client.setHttps(true);
+            client.post();
+
+            String xml = client.getContent();
+            Map<String,String> resultMap = WXPayUtil.xmlToMap(xml);
+            return resultMap;
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    @Override
+    public void updateOrderStatus(Map<String, String> map) {
+        String orderNo = map.get("out_trade_no");
+        QueryWrapper<Order> wrapper = new QueryWrapper<>();
+        wrapper.eq("order_no",orderNo);
+        Order order = orderService.getOne(wrapper);
+
+        if(order.getStatus().intValue() == 1){return;}
+
+        orderService.updateById(order);
+
+        PayLog payLog = new PayLog();
+        payLog.setOrderNo(orderNo);
+        payLog.setPayTime(new Date());
+        payLog.setPayType(1);//支付类型
+        payLog.setTotalFee(order.getTotalFee());//总金额(分)
+        payLog.setTradeState(map.get("trade_state"));//支付状态
+        payLog.setTransactionId(map.get("transaction_id"));
+        payLog.setAttr(JSONObject.toJSONString(map));
+
+        baseMapper.insert(payLog);//插入到支付日志表
+    }
 }
